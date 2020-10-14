31b62198cea55bbf5593d28b611663f9e4f914df
Shunsuke Omura <tug99901@temple.edu>
2020-10-14 12:42:04 -0400

    resoleved merge conf

--- image_process.py
+++ image_process.py
@@ -18,6 +18,23 @@ class ImageProcess:
 
     def square(self, image):
         """ SQURE FUNCTION """
+        height = image.shape[0]
+        width  = image.shape[1]
+        white = [255, 255, 255]
+
+        if height != width:
+            if height > width:
+                dif = height - width
+                squared = cv2.copyMakeBorder(image, 0, 0, int(dif/2), int(dif/2), cv2.BORDER_CONSTANT, value=white)
+                # print(squared.shape)
+                return squared 
+            else:
+                dif = width - height
+                squared = cv2.copyMakeBorder(image, int(dif/2), int(dif/2), 0, 0, cv2.BORDER_CONSTANT, value=white)
+                # print(squared.shape)
+                return squared
+        else:
+            return image
 
     def grayscale(self, image):
         """ GRAYSCALE FUNCTION """
@@ -27,10 +44,10 @@ class ImageProcess:
 
 
 if __name__ == '__main__':
-    sampleImage = cv2.imread("dataset_original/treadmill/treadmill_001.jpg")
+    sampleImage = cv2.imread("dataset_original/treadmill/treadmill_004.jpg")
     print(sampleImage.shape)
     test = ImageProcess()
 
-    # Resize Test
-    resized = test.resize(sampleImage)
-    print(resized.shape)
+    # Square Test
+    squared = test.square(sampleImage)
+    cv2.imwrite("sample.jpg", squared)
