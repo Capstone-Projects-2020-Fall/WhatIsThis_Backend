9dd321dc95e6b0f0c648270ccf15a80d1fb47c06
Jesse C. Yentsch <yentschj@gmail.com>
2020-12-01 16:27:34 -0500

    Resolve directory issues

--- containerized/app.py
+++ containerized/app.py
@@ -25,8 +25,11 @@ import classifier
 # Flask Modules
 from flask import Flask, request, Response
 
-# Variables
-MODEL_DIR = os.path.join(os.getcwd(), "model")
+# Directory Variables
+current_directory = os.getcwd()
+
+if 'containerized' not in current_directory :
+    DIRECTORY_PATH = current_directory + '/containerized'
 
 MAP = {0:'bench',
        1:'benchpress',
@@ -63,7 +66,7 @@ def predict():
     bodyJson = request.get_json(force=True).get('body')
     data = bodyJson.get('imgsource')
 
-    path = "./temp/test.png"
+    path = DIRECTORY_PATH + "/temp/test.png"
     # Decode the BASE64 image and save it into the temp file
     imgdata = base64.b64decode(str(data))
     image = Image.open(BytesIO(imgdata))
@@ -72,19 +75,19 @@ def predict():
 
     r = requests.post(
         'https://api.remove.bg/v1.0/removebg',
-        files={'image_file': open('./temp/test.png', 'rb')},
+        files={'image_file': open(DIRECTORY_PATH + '/temp/test.png', 'rb')},
         data={'size': 'auto', 'bg_color': 'white'},
         headers={'X-Api-Key': 'iTUtE8hsnt76HMLmfjPAi2hp'},
     )
     if r.status_code == requests.codes.ok:
-        with open('./temp/test.png', 'wb') as out:
+        with open(DIRECTORY_PATH + '/temp/test.png', 'wb') as out:
             out.write(r.content)
     else:
         print("Error:", r.status_code, r.text)
 
     # Call the image recognition function
     predictor = classifier.Classifier()
-    predicted_label = predictor.image_recognition('./temp/test.png')
+    predicted_label = predictor.image_recognition(DIRECTORY_PATH + '/temp/test.png')
     print(predicted_label)
 
     max_index = np.argmax(predicted_label[0])
